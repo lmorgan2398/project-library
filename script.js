fa98913525e829b4f8bdd51023bc62099ea2c864
lmorgan2398 <lmorgan2398@gmail.com>
2025-02-18 22:10:04 -0600

    Add book classes and generate pseudocode for display function

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const myLibrary = [];
+const myLibrary = new Array(36).fill(null);
+console.log(myLibrary);
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, status, color) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
+    this.color = color;
 };
 
-function addBookToLibrary(title, author, pages, status) {
-    let newBook = new Book(title, author, pages, status);
+function addBookToLibrary(title, author, pages, status, color) {
+    let newBook = new Book(title, author, pages, status, color);
     myLibrary.push(newBook);
 }
 
 function displayLibrary() {
-    for (let i = 0; i < myLibrary.length; i++);
-    let newBook = myLibrary[i];
+    for (let i = 0; i < myLibrary.length; i++); {
+    (function (currentIndex) {
+        let currentBookObj = myLibrary[currentIndex];
+        let currentBookEle = document.querySelector(`.book${currentIndex}`);
+        if(currentBookObj === null) {
+            currentBookEle.addEventListener('click', {
+                // code to be inserted representing the 'add new book' dialog
+                // will change currentBookObj to the value of the inputs
+                // then update display to match input values
+            })
+            // set class on the element to highlight and add a plus sign when hovered
+        } else {
+            currentBookEle.addEventListener('click', {
+                // code to be inserted representing a current book obj dialog
+                // will display information from library array objects
+                // will give options to delete the object and update display
+            })
+            // set class to display book color and title and grow book when hovered
+        }
+    })(i);
+    }
+};
+
     
-}
+
