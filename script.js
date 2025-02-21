fea79dd179a985ad1650d91596944f3e01bc09b9
lmorgan2398 <lmorgan2398@gmail.com>
2025-02-20 21:46:15 -0600

    Change classes to data values for query selector and added sample objects
    Sample objects will help with styling books before user input, and data
    classes are more appropriate for associating indices with elements on the
    DOM

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const myLibrary = new Array(36).fill(null);
-console.log(myLibrary);
 
 function Book(title, author, pages, status, color) {
     this.title = title;
@@ -9,16 +8,25 @@ function Book(title, author, pages, status, color) {
     this.color = color;
 };
 
-function addBookToLibrary(title, author, pages, status, color) {
+function addBookToLibrary(title, author, pages, status, color, index) {
     let newBook = new Book(title, author, pages, status, color);
-    myLibrary.push(newBook);
+    myLibrary[`${index}`] = newBook;
 }
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '310', 'read', 'red', 17);
+addBookToLibrary('The Lord of the Rings: The Fellowship of the Ring', 'J.R.R. Tolkien', '479', 'read', 'green', 18)
+addBookToLibrary('The Lord of the Rings: The Two Towers', 'J.R.R. Tolkien', '415', 'read', 'yellow', 19)
+addBookToLibrary('The Lord of the Rings: The Return of the King', 'J.R.R. Tolkien', '347', 'read', 'blue', 20)
+
+console.log(myLibrary);
+
+
 
 function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
     (function (currentIndex) {
         let currentBookObj = myLibrary[currentIndex];
-        let currentBookEle = document.querySelector(`.book${currentIndex}`);
+        let currentBookEle = document.querySelector(`[data-index-number="${currentIndex}"]`);
         if(currentBookObj === null) {
             currentBookEle.addEventListener('click', {
                 // code to be inserted representing the 'add new book' dialog
